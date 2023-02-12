a6e1d36e14fa4e640a6af6671af72a4fcefe64d0
Sairam Sah <sairam2054@gmail.com>
2023-02-12 22:30:09 +0545

    Converterd to Grayscale and Blur

---
+++ basic.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+img = cv.imread('Photos/cat.webp')
+
+cv.imshow('cat', img)
+
+#converting  to grayscale
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+
+# Blur 
+blur = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
+cv.imshow('Blur',blur)
+
+cv.waitKey(0)
