56bf08b35de3fd01a5f9c62c973991817b646981
Sairam Sah <sairam2054@gmail.com>
2023-02-13 19:13:57 +0545

    converting to grayscale,blur,edge cascade,dilating,eroding,resizing,crapping

--- basic.py
+++ basic.py
@@ -9,8 +9,28 @@ cv.imshow('cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray',gray)
 
-# # Blur 
-# blur = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
-# cv.imshow('Blur',blur)
+ # Blur 
+blur = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
+cv.imshow('Blur',blur)
 
+# Edge cascade
+canny = cv.Canny( img, 125,175)
+cv.imshow('Canny Edges', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (3,3), iterations=1)
+cv.imshow('dilated', dilated)
+
+# Erroding
+eroded = cv.erode(dilated, (3,3),iterations=1)
+cv.imshow('eroded', eroded)
+
+# Resize
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('resized', resized)
+
+
+# crapping
+cropped = img[50:200,100:200]
+cv.imshow('Cropped', cropped)
 cv.waitKey(0)
