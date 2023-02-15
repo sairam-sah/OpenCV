3f99f7440827452b91420b6508d291b09f599d64
sairam-sah <85270513+sairam-sah@users.noreply.github.com>
2023-02-15 18:51:10 +0545

    reading, resizing image and videos

--- rescale.py
+++ rescale.py
@@ -1,28 +1,24 @@
 from logging import captureWarnings
 import cv2 as cv
 
-img = cv.imread('photos/cat.webp')
 
-cv.imshow('cat', img)
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale) 
 
-
-# def rescaleFrame(frame, scale=0.75):
-#     width = int(frame.shape[1] * scale)
-#     height = int(frame.shape[0] * scale) 
-
-#     dimensions = (width, height)
+    dimensions = (width, height)
     
-#     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 # resized_image = rescaleFrame(img)
 # cv.imshow('Image', resized_image)
 # def changeRes(width,height):
     
     #Reading videos
- capture = cv.VideoCapture('videos/baby.mp4')
+capture = cv.VideoCapture('videos/baby.mp4')
 
 while True:
-     isTrue, frame  =captureWarnings .read()
+     isTrue, frame  =capture.read()
 
      frame_resized = rescaleFrame(frame,scale=0.3)
 
@@ -34,6 +30,5 @@ while True:
 
 capture.release()
 cv.destroyWindow()
-
 
 cv.waitKey(0)
