bc72e255d6dcf76e5204089a804ec5a56f410022
Sairam Sah <sairam2054@gmail.com>
2023-02-16 18:35:53 +0545

    bitwise and,or,not,xor

---
+++ bitwise.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400,400), dtype='uint8')
+
+rectangle = cv.rectangle(blank.copy(),(30,30),(350,350),255,-1)
+circle = cv.circle(blank.copy(),(200,200),200,255,-1)
+
+cv.imshow('Rectangle',rectangle)
+cv.imshow('Circle',circle)
+
+# bitwise AND--> intersecting region
+bitwise_and =cv.bitwise_and(rectangle, circle)
+cv.imshow('Bitwise_and',bitwise_and)
+
+# bitwise OR--> non intersecting and intersecting region
+bitwise_or = cv.bitwise_or(rectangle,circle)
+cv.imshow('Bitwise_or',bitwise_or)
+
+# BIteise XOR-->non intersecting regions
+bitwise_xor =cv.bitwise_xor(rectangle,circle)
+cv.imshow('Bitwise_XOR',bitwise_xor)
+
+# bitwise NOT
+bitwise_not = cv.bitwise_not(rectangle,circle)
+cv.imshow('Bitwise_not',bitwise_not)
+
+
+
+
+
+cv.waitKey(0)
